83f4bcd776d0c330bde6fb55c22b4acb5afc743e
Derek Rada <derekrada@gmail.com>
2020-01-06 17:33:43 -0800

    feat: Added pre-release and release scripts

---
+++ src/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+. /common.sh
+
+export INPUT_TF_DOCS_GIT_COMMIT_MESSAGE="chore: automated release process"
+cd $GITHUB_WORKSPACE
+git_setup
+
+VERSION=`gomplate -d meta=.github/meta.yml 'v{{ (ds "meta").major_version }}'`
+git tag -f "${VERSION}"
+git push --tags
