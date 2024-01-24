3df7ebb7c073749d9f2dd688fb9720a9684b9ce3
kamiKAC <kamiKAC@users.noreply.github.com>
2024-01-24 22:27:15 +0100

    change OUTPUT_FILE -> INPUT_OUTPUT_FILE
    Signed-off-by: kamiKAC <m.k.rudzki@gmail.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -147,7 +147,7 @@ update_doc() {
     fi
 
     if [ "${INPUT_OUTPUT_METHOD}" == "inject" ] || [ "${INPUT_OUTPUT_METHOD}" == "replace" ]; then
-        git_add "${working_dir}/${OUTPUT_FILE}"
+        git_add "${working_dir}/${INPUT_OUTPUT_FILE}"
     fi
 }
 
