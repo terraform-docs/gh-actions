43f9c391730252b72278c8ef40caf1f3d4207609
Khosrow Moossavi <khos2ow@gmail.com>
2024-06-04 10:11:02 -0400

    build: overall improvements to release scripts
    Signed-off-by: Khosrow Moossavi <khos2ow@gmail.com>

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -24,10 +24,10 @@ TF_DOCS_VERSION=$2
 PWD=$(cd "$(dirname "$0")" && pwd -P)
 
 if [ -z "${NEW_VERSION}" ]; then
-    NEW_VERSION=$(grep "uses: terraform-docs/gh-actions" "${PWD}"/../README.md | tr -s ' ' | uniq | cut -d"@" -f2)
+    NEW_VERSION=$(grep "uses: terraform-docs/gh-actions" "${PWD}"/../../README.md | tr -s ' ' | uniq | cut -d"@" -f2)
 fi
 if [ -z "${NEW_VERSION}" ]; then
-    echo "Usage: update-readme.sh <NEW_VERSION> <TF_DOCS_VERSION>"
+    echo "Usage: pre-release.sh <NEW_VERSION> <TF_DOCS_VERSION>"
     exit 1
 fi
 
@@ -35,15 +35,29 @@ if [ -z "${TF_DOCS_VERSION}" ]; then
     TF_DOCS_VERSION=$(grep "FROM quay.io/terraform-docs/terraform-docs" "${PWD}"/../Dockerfile | tr -s ' ' | uniq | cut -d":" -f2)
 fi
 if [ -z "${TF_DOCS_VERSION}" ]; then
-    echo "Usage: update-readme.sh <NEW_VERSION> <TF_DOCS_VERSION>"
+    echo "Usage: pre-release.sh <NEW_VERSION> <TF_DOCS_VERSION>"
     exit 1
 fi
 
-# Update the README
+# Update README
 VERSION=v${NEW_VERSION//v/} TERRAFORM_DOCS_VERSION=v${TF_DOCS_VERSION//v/} \
-    gomplate -d \
-    action="${PWD}"/../action.yml \
-    -f "${PWD}"/../.github/templates/README.tpl \
-    -o "${PWD}"/../README.md
+    gomplate \
+    -d action="${PWD}"/../../action.yml \
+    -f "${PWD}"/../../.github/templates/README.tpl \
+    -o "${PWD}"/../../README.md
 
-echo "README.md updated."
+# Update Dockerfile
+sed -i -E "s|FROM quay.io/terraform-docs/terraform-docs:(.*)|FROM quay.io/terraform-docs/terraform-docs:${TF_DOCS_VERSION//v/}|g" "${PWD}"/../../Dockerfile
+
+# Update action.yml
+sed -i -E "s|docker://quay.io/terraform-docs/gh-actions:(.*)\"|docker://quay.io/terraform-docs/gh-actions:${NEW_VERSION//v/}\"|g" "${PWD}"/../../action.yml
+
+if [ "$(git status --porcelain | grep -c 'M README.md')" -eq 1 ]; then
+    echo "Modified: README.md"
+fi
+if [ "$(git status --porcelain | grep -c 'M Dockerfile')" -eq 1 ]; then
+    echo "Modified: Dockerfile"
+fi
+if [ "$(git status --porcelain | grep -c 'M action.yml')" -eq 1 ]; then
+    echo "Modified: action.yml"
+fi
