e81cf88ff859a24b1cae2b5e2126e157178ca4e7
coangel <coangel@cisco.com>
2025-03-04 15:57:56 -0500

    Add debug line to test

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -189,7 +189,7 @@ if [ "${INPUT_GIT_PUSH}" = "true" ]; then
     git push
 else
     if [ "${INPUT_FAIL_ON_DIFF}" = "true" ] && [ "${num_changed}" -ne 0 ]; then
-        echo "::error ::Uncommitted change(s) has been found!"
+        printf "::error :: %s Uncommitted change(s) have been found" "$(git status)"
         exit 1
     fi
 fi
